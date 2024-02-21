6c043b38862771a7fd4d9b74de82bc622415ef03
chotelal patel <chhotupatel705@gmail.com>
2024-02-21 22:43:57 +0530

    change

--- frontend/src/Pages/Login.js
+++ frontend/src/Pages/Login.js
@@ -26,7 +26,7 @@ const Login = () => {
       console.log(userData);
 
       // http://localhost:8000
-      //helloo @
+      //helloo @123
      const res=await axios.post('https://ai-chatgpt-clone-g51r7q9tq-chhotelals-projects.vercel.app/api/v1/auth/login', userData);
       //const res = await axios.post('https://ai-chatgpt-clone-14jojxpqu-chhotelals-projects.vercel.app/api/v1/auth/login', userData);
 
