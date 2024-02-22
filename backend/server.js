49fa3257ef549a82a159b1cac89eb2ff6ea99711
chotelal patel <chhotupatel705@gmail.com>
2024-02-22 14:47:19 +0530

    hj

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,7 @@ const authRoutes=require('./routes/authRoutes');
 // To allow specific origins
 const corsOptions = {
   origin: 'https://65d70f9a04d0dc7c2bbf1976--endearing-mermaid-007f3f.netlify.app',
+  Credential:true
 };
 
 
